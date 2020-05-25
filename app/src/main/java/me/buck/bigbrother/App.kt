d4d2953ae49563763f7cc33fb049dd38988bb772
buck <coderbuck@163.com>
2020-05-25 12:42:01 +0800

    test

--- app/src/main/java/me/buck/bigbrother/App.kt
+++ app/src/main/java/me/buck/bigbrother/App.kt
@@ -1,16 +1,25 @@
 package me.buck.bigbrother
 
 import android.app.Application
+import com.blankj.utilcode.util.CrashUtils
+import com.blankj.utilcode.util.Utils
 import com.jakewharton.threetenabp.AndroidThreeTen
 import timber.log.Timber
 
+@Suppress("unused")
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
         AndroidThreeTen.init(this)
         ObjectBox.init(this)
-        Timber.plant(Timber.DebugTree())
+        Timber.plant(object : Timber.DebugTree() {
+            override fun createStackElementTag(element: StackTraceElement): String? {
+                return "kd-" + super.createStackElementTag(element)
+            }
+        })
+        Utils.init(this)
+        CrashUtils.init(filesDir.absolutePath + "/crash/")
     }
 
 }
--- app/src/main/java/me/buck/bigbrother/test/TestActivity.kt
+++ app/src/main/java/me/buck/bigbrother/test/TestActivity.kt
@@ -2,6 +2,9 @@ package me.buck.bigbrother.test
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.blankj.utilcode.constant.PermissionConstants
+import com.blankj.utilcode.util.PermissionUtils
+import com.blankj.utilcode.util.Utils
 import io.github.coderbuck.boring.util.contentView
 import me.buck.bigbrother.R
 import me.buck.bigbrother.databinding.ActivityTestBinding
@@ -14,5 +17,22 @@ class TestActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
         bind = ActivityTestBinding.bind(contentView)
+
+        PermissionUtils.permission(PermissionConstants.CALENDAR)
+            .rationale(PermissionUtils.OnRationaleListener { activity, shouldRequest ->
+            })
+            .callback(object : PermissionUtils.FullCallback {
+                override fun onGranted(granted: MutableList<String>) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onDenied(
+                    deniedForever: MutableList<String>,
+                    denied: MutableList<String>
+                ) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+            })
+            .request()
     }
 }
