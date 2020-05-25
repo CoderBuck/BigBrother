e921464ed7a0ee6b36d8013e96f288747cf64356
buck <coderbuck@163.com>
2020-05-25 12:20:23 +0800

    clean

--- app/src/main/java/me/buck/bigbrother/test/TestActivity.kt
+++ app/src/main/java/me/buck/bigbrother/test/TestActivity.kt
@@ -2,15 +2,13 @@ package me.buck.bigbrother.test
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import io.github.coderbuck.boring.util.contentView
 import me.buck.bigbrother.R
 import me.buck.bigbrother.databinding.ActivityTestBinding
 
 class TestActivity : AppCompatActivity() {
 
-    private lateinit var bind:ActivityTestBinding
+    private lateinit var bind: ActivityTestBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
